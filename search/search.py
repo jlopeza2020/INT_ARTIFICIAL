b0cc92b6e93227c36a09bd41af2d1f3a71333945
jlopeza2020 <j.lopeza.2020@alumnos.urjc.es>
2022-10-06 15:39:06 +0200

    done exe3

--- search/search.py
+++ search/search.py
@@ -268,6 +268,7 @@ def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
     #util.raiseNotDefined()
+    
 
 
 # Abbreviations
